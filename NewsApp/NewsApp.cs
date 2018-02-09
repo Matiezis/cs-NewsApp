830ff686af76753f6f2b4a2b43b2ab471992411e
Matiezis <matiezisu@gmail.com>
2018-02-09 11:53:24 +0100

    Reading website links from file or by FileDialog.

--- NewsApp/NewsApp.cs
+++ NewsApp/NewsApp.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace NewsApp
 {
@@ -18,11 +19,63 @@ namespace NewsApp
             InitializeComponent();
         }
 
+        private string websiteFilePath = "";
+        private string articlesFilePath = "";
+        List<string> websiteList = new List<string>();
+
         private void NewsApp_Load(object sender, EventArgs e)
         {
-            WebClient x = new WebClient();
-            string source = x.DownloadString("http://www.onet.pl/");
-            rtbInput.Text = source;
+            getSiteList();
+            //WebClient x = new WebClient();
+            //string source = x.DownloadString("http://www.onet.pl/");
+        }
+
+        private string SelectFile()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            fileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            fileDialog.FilterIndex = 2;
+            fileDialog.RestoreDirectory = true;
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    return fileDialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Nie udało się otworzyć pliku. Original error: " + ex.Message);
+                }
+            }
+            return "";
+        }
+
+        private void readSiteList()
+        {
+            if (File.Exists(websiteFilePath))
+            {
+                websiteList = new List<string>(File.ReadLines(websiteFilePath));
+            }
+        }
+
+        private void getSiteList()
+        {
+            string localDirectory = Directory.GetCurrentDirectory();
+
+            if (File.Exists(localDirectory + "\\sites.txt"))
+            {
+                websiteFilePath = localDirectory + "\\sites.txt";
+            }
+            else
+                websiteFilePath = SelectFile();
+
+            readSiteList();
+            foreach (string s in websiteList)
+            {
+                rtbInput.AppendText(s + Environment.NewLine);
+            }
         }
     }
 }
