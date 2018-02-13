c44ee1f32edc8c12190f5c9136bd7868f1b404ce
Matiezis <matiezisu@gmail.com>
2018-02-13 13:12:01 +0100

    Simple filtered article list sending via Email.

--- NewsApp/NewsApp.cs
+++ NewsApp/NewsApp.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
+using System.Net.Mail;
 
 namespace NewsApp
 {
@@ -100,19 +101,18 @@ namespace NewsApp
                     if (document.DocumentNode.SelectNodes("//a") != null)
                     {
                         HtmlNode[] nodes = document.DocumentNode.SelectNodes("//a").ToArray();
-                        foreach (HtmlNode a in nodes) { 
-                        if (a.Attributes.Contains("href"))
-                        //Get your value here
+                        foreach (HtmlNode node in nodes) { 
+                        if (node.Attributes.Contains("href"))
                         {
-                                tB.AppendText(a.InnerText.Trim().Replace("&quot;", "'").ToLower() + Environment.NewLine + a.Attributes["href"].Value.Trim() +Environment.NewLine);
-                            siteList[siteList.Count - 1].articles.Add(new Article(a.InnerText.Trim().Replace("&quot;", "'").ToLower(), a.Attributes["href"].Value.Trim()));
+                                tB.AppendText(node.InnerText.Trim().Replace("&quot;", "'").ToLower() + Environment.NewLine + node.Attributes["href"].Value.Trim() +Environment.NewLine);
+                            siteList[siteList.Count - 1].articles.Add(new Article(node.InnerText.Trim().Replace("&quot;", "'").ToLower(), node.Attributes["href"].Value.Trim()));
                         }
                     }
                     }
                 }
                 catch(Exception e)
                 {
-
+                    MessageBox.Show("Following error occured: "+Environment.NewLine+e.ToString());
                 }
                 tP.Controls.Add(tB);
                 tC.TabPages.Add(tP);
@@ -141,32 +141,61 @@ namespace NewsApp
                 MessageBox.Show("No Input");
             }
         }
+
+        private void bttnSend_Click(object sender, EventArgs e)
+        {
+            if (tbMailInput.Text != ""&& tbEmailAdress.Text !="" && tbEmailLogin.Text !="" && tbEmailPassword.Text !="")
+            {
+
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    SmtpClient SmtpServer = new SmtpClient("smtp-mail.outlook.com");
+
+                    mail.From = new MailAddress(tbEmailAdress.Text);
+                    mail.To.Add(tbMailInput.Text);
+                    mail.Subject = "Articles for keyword: "+tbKeyword.Text;
+                    mail.Body = rtbOutput.Text;
+
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential(tbEmailLogin.Text, tbEmailPassword.Text);
+                    SmtpServer.EnableSsl = true;
+
+                    SmtpServer.Send(mail);
+                    MessageBox.Show("Mail succesfuly sent");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
         /*
 private void test()
 {
-   const string xpath = "//a";
-   HtmlWeb web = new HtmlWeb();
-   List<Site> siteList = new List<Site>();
-   siteList.Add(new Site("onet"));
+const string xpath = "//a";
+HtmlWeb web = new HtmlWeb();
+List<Site> siteList = new List<Site>();
+siteList.Add(new Site("onet"));
 
-   HtmlAgilityPack.HtmlDocument document = web.Load("http://www.onet.pl/");
-   //The following gives you a node collection of your two <a> elements
-   HtmlNode[] items = document.DocumentNode.SelectNodes(xpath).ToArray();
-   foreach (HtmlNode a in items)
-   {
-       if (a.Attributes.Contains("href"))
-       //Get your value here
-       {
-           siteList[siteList.Count - 1].articles.Add(new Article(a.InnerText.Trim().Replace("&quot;", "'"), a.Attributes["href"].Value));
-       }
-   }
-   foreach(Site site in siteList)
-   {
-       foreach(Article article in site.articles)
-       {
-           rtbInput.AppendText(article.name + Environment.NewLine + article.link + Environment.NewLine);
-       }
-   }
+HtmlAgilityPack.HtmlDocument document = web.Load("http://www.onet.pl/");
+//The following gives you a node collection of your two <a> elements
+HtmlNode[] items = document.DocumentNode.SelectNodes(xpath).ToArray();
+foreach (HtmlNode a in items)
+{
+if (a.Attributes.Contains("href"))
+//Get your value here
+{
+  siteList[siteList.Count - 1].articles.Add(new Article(a.InnerText.Trim().Replace("&quot;", "'"), a.Attributes["href"].Value));
+}
+}
+foreach(Site site in siteList)
+{
+foreach(Article article in site.articles)
+{
+  rtbInput.AppendText(article.name + Environment.NewLine + article.link + Environment.NewLine);
+}
+}
 }
 */
     }
--- NewsApp/Program.cs
+++ NewsApp/Program.cs
@@ -15,7 +15,7 @@ namespace NewsApp
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(false);            
             Application.Run(new NewsApp());
         }
     }
